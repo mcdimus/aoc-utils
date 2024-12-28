0e7b4b501d3cbb75d19678dc9f700298fe83eac9
Dmitri Maksimov <mcdimus@gmail.com>
2024-12-29 00:40:55 +0200

    docs: Better KDoc

--- src/main/kotlin/com/github/mcdimus/aoc/utils/StringX.kt
+++ src/main/kotlin/com/github/mcdimus/aoc/utils/StringX.kt
@@ -1,5 +1,10 @@
 package com.github.mcdimus.aoc.utils
 
+/**
+ * Computes the MD5 hash of this string.
+ *
+ * @return The MD5 hash of this string as a hexadecimal string.
+ */
 @OptIn(ExperimentalStdlibApi::class)
 fun String.md5(): String {
   val md = java.security.MessageDigest.getInstance("MD5")
--- src/main/kotlin/com/github/mcdimus/aoc/utils/util_functions.kt
+++ src/main/kotlin/com/github/mcdimus/aoc/utils/util_functions.kt
@@ -4,8 +4,12 @@ import java.io.BufferedReader
 import java.io.FileNotFoundException
 
 /**
- * Reads lines from the given input txt file.
- * The file should be located in the classpath and match `$name.txt` name.
+ * Reads lines from the given input text file.
+ * The file should be located in the classpath and match the `$name.txt` name.
+ *
+ * @param name The name of the file (without the `.txt` extension) to read from the classpath.
+ * @return A list of strings, each representing a line from the file.
+ * @throws FileNotFoundException If the file is not found in the classpath.
  */
 fun readInput(name: String): List<String> = object {}.javaClass.getResourceAsStream("/$name.txt")?.use {
   it.bufferedReader().use(BufferedReader::readLines)
