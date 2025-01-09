c7136724697c189456f967b48f0bb14f39f62c55
Dmitri Maksimov <mcdimus@gmail.com>
2025-01-10 01:04:48 +0200

    feat: Add function to calculate the prime factors of the given number

--- src/main/kotlin/com/github/mcdimus/aoc/utils/math/MoreMath.kt
+++ src/main/kotlin/com/github/mcdimus/aoc/utils/math/MoreMath.kt
@@ -1,5 +1,7 @@
 package com.github.mcdimus.aoc.utils.math
 
+import kotlin.math.sqrt
+
 /**
  * Provides a collection of utility methods for advanced mathematical operations.
  */
@@ -43,5 +45,36 @@ object MoreMath {
    * @return The greatest common divisor of the given numbers.
    */
   fun gcd(a: Long, b: Long) = gcd.gcd(a, b)
+
+  /**
+   * Returns the prime factors of the given number.
+   *
+   * @param number The number to factorize.
+   * @return A prime factors list of the given number.
+   * If the number is less than or equal to zero, an empty list is returned.
+   */
+  fun primeFactorsOf(number: Long): List<Long> {
+    if (number <= 0) {
+      return emptyList()
+    }
+    return buildList {
+      var n = number
+      while (n % 2 == 0L) {
+        add(2L)
+        n /= 2
+      }
+
+      val squareRoot = sqrt(n.toDouble()).toLong()
+      for (i in 3..squareRoot step 2) {
+        while (n % i == 0L) {
+          add(i)
+          n /= i
+        }
+      }
+      if (n > 2) {
+        add(n)
+      }
+    }
+  }
 
 }
--- src/test/kotlin/com/github/mcdimus/aoc/utils/math/MoreMathTest.kt
+++ src/test/kotlin/com/github/mcdimus/aoc/utils/math/MoreMathTest.kt
@@ -13,4 +13,30 @@ class MoreMathTest {
     assertThat(MoreMath.gcd(longArrayOf(12L, 18L, 60L))).isEqualTo(6)
   }
 
+  @Test
+  fun primeFactorsOfPrimeNumber() {
+    assertThat(MoreMath.primeFactorsOf(13L)).containsExactly(13L)
+  }
+
+  @Test
+  fun primeFactorsOfCompositeNumber() {
+    assertThat(MoreMath.primeFactorsOf(60L)).containsExactly(2L, 2L, 3L, 5L)
+    assertThat(MoreMath.primeFactorsOf(54235234562L)).containsExactly(2L, 89L, 587L, 519067L)
+  }
+
+  @Test
+  fun primeFactorsOfOne() {
+    assertThat(MoreMath.primeFactorsOf(1L)).isEmpty()
+  }
+
+  @Test
+  fun primeFactorsOfZero() {
+    assertThat(MoreMath.primeFactorsOf(0L)).isEmpty()
+  }
+
+  @Test
+  fun primeFactorsOfNegativeNumber() {
+    assertThat(MoreMath.primeFactorsOf(-60L)).isEmpty()
+  }
+
 }
